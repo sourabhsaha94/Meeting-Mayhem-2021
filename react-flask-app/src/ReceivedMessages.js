367967814837d8c467ec30cd0e6a919c98b9c7ff
Akriti Anand <akriti.anand15@gmail.com>
2021-04-13 16:04:10 -0400

    received

--- react-flask-app/src/ReceivedMessages.js
+++ react-flask-app/src/ReceivedMessages.js
@@ -1,11 +1,40 @@
+
 import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+
 export default function ReceivedMessages() {
 
-    
+
+
+    const columns = [{
+        dataField: 'id',
+        text: 'Product ID'
+      }, {
+        dataField: 'name',
+        text: 'Product Name'
+      }, {
+        dataField: 'price',
+        text: 'Product Price'
+      }];
+
+      var products = [{
+        id: 1,
+        name: "Product1",
+        price: 120
+    }, {
+        id: 2,
+        name: "Product2",
+        price: 80
+    }];
+
 
     return(
-        <div>
-            You are on Received Messages page
-        </div>
+        <BootstrapTable keyField='id' data={ products } columns={ columns } />
     ); 
 }
+
+
+
+
+
+
