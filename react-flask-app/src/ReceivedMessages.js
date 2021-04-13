6f87ab212a6775d97b9fed8df5351b944ae7eb21
Akriti Anand <akriti.anand15@gmail.com>
2021-04-13 17:10:44 -0400

    installed table

--- react-flask-app/src/ReceivedMessages.js
+++ react-flask-app/src/ReceivedMessages.js
@@ -1,37 +1,37 @@
 
-// import React from "react";
-// import BootstrapTable from 'react-bootstrap-table-next';
+import React from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
 
-// export default function ReceivedMessages() {
-
-
-
-//     const columns = [{
-//         dataField: 'id',
-//         text: 'Product ID'
-//       }, {
-//         dataField: 'name',
-//         text: 'Product Name'
-//       }, {
-//         dataField: 'price',
-//         text: 'Product Price'
-//       }];
-
-//       var products = [{
-//         id: 1,
-//         name: "Product1",
-//         price: 120
-//     }, {
-//         id: 2,
-//         name: "Product2",
-//         price: 80
-//     }];
+export default function ReceivedMessages() {
 
 
-//     return(
-//         <BootstrapTable keyField='id' data={ products } columns={ columns } />
-//     ); 
-// }
+
+    const columns = [{
+        dataField: 'id',
+        text: 'Product ID'
+      }, {
+        dataField: 'name',
+        text: 'Product Name'
+      }, {
+        dataField: 'price',
+        text: 'Product Price'
+      }];
+
+      var products = [{
+        id: 1,
+        name: "Product1",
+        price: 120
+    }, {
+        id: 2,
+        name: "Product2",
+        price: 80
+    }];
+
+
+    return(
+        <BootstrapTable keyField='id' data={ products } columns={ columns } />
+    ); 
+}
 
 
 
