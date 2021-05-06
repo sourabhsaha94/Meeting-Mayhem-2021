e55d5fb6b651f77d1e5a25d4b2de2849a79b6180
eyeclept <hi333dog@gmail.com>
2021-05-05 20:21:54 -0400

    added login_mm file for all things login / logout

---
+++ api/venv/Login_MM.py
@@ -0,0 +1,16 @@
+
+"""
+File:    DB_MM.py
+Author:  Julia Nau, Richard Baldwin
+Date:    5/1/2021
+E-mail: jnau1@umbc.edu
+		richardbaldwin@umbc.edu
+Description: this file manages login. it is to be called my Routes_MM.py
+			It interfaces with DB_PW_MM.py
+
+"""
+
+class MM_Login():
+	"""
+	
+	"""
