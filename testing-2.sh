a1cafaee84d8cb7cfdea757b5b405c8ee8ef3eab
Sourabh Saha <sssaha2@ncsu.edu>
2021-06-11 19:36:58 -0700

    testing script

--- testing-2.sh
+++ testing-2.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 START="$(date +%s%3N)"
-curl http://localhost:5000/getrecipients
+#get recipients
+#curl http://3.133.147.109:5000/getrecipients
+#login
+#json="{\"email\":\"sourabh\",\"password\":\"password\"}"
+#curl --header "Content-Type:application/json" -d "$json" http://3.133.147.109:5000/login
+#send message
+#json="{\"sender\":\"akritia1@umbc.edu\",\"receiver\":\"akritia1@umbc.edu\",\"message\":\"hello\"}"
+#curl --header "Content-Type:application/json" -d "$json" http://3.133.147.109:5000/sendmessage
+#get message
+curl http://3.133.147.109:5000/getmessages?receiver=akritia1@umbc.edu
 DURATION=$[ $(date +%s%3N) - ${START} ]
 echo ${DURATION}
